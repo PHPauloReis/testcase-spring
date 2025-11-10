4567807811de4c56f9000ddc5f825bb9432da38c
Paulo Reis dos Santos <paulo.santos@prodeb.ba.gov.br>
2025-11-10 16:22:28 -0300

    Commit inicial

---
+++ src/main/java/br/gov/ba/prodeb/testecase/TestecaseApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ba.prodeb.testecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestecaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ba/prodeb/testecase/TestecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ba.prodeb.testecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestecaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
